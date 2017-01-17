241599c6e08d1a02d964a0a0e1ba5168285c1180
notryanb <notryanb@gmail.com>
2017-01-16 22:41:13 -0500

    Add automated movement

--- main.rb
+++ main.rb
@@ -26,30 +26,46 @@ def main
     char = STDIN.read_nonblock(1) rescue nil
     current_position = player.current_position
 
-    if char =~ /a/i # Left
+    if player.current_direction == :left
       if player.current_position[0] > 0
         player.set_current_position(current_position[0] - 1, current_position[1])
         board.board[player.current_position[1]][player.current_position[0]] = player
         board.board[current_position[1]][current_position[0]] = '.'
       end
-    elsif char =~ /d/i #right
+    end
+
+    if player.current_direction == :right
       if (player.current_position[0] + 1) < columns
         player.set_current_position(current_position[0] + 1, current_position[1])
         board.board[player.current_position[1]][player.current_position[0]] = player
         board.board[current_position[1]][current_position[0]] = '.'
       end
-    elsif char =~ /s/i #down
+    end
+
+    if player.current_direction == :down
       if (player.current_position[1] + 1) < rows
         player.set_current_position(current_position[0], current_position[1] + 1)
         board.board[player.current_position[1]][player.current_position[0]] = player
         board.board[current_position[1]][current_position[0]] = '.'
       end
-    elsif char =~ /w/i #up
+    end
+
+    if player.current_direction == :up
       if (player.current_position[1] - 1) >= 0
         player.set_current_position(current_position[0], current_position[1] -1)
         board.board[player.current_position[1]][player.current_position[0]] = player
         board.board[current_position[1]][current_position[0]] = '.'
       end
+    end
+
+    if char =~ /a/i # Left
+      player.set_current_direction(:left)
+    elsif char =~ /d/i #right
+      player.set_current_direction(:right)
+    elsif char =~ /s/i #down
+      player.set_current_direction(:down)
+    elsif char =~ /w/i #up
+      player.set_current_direction(:up)
     end
 
    
--- player.rb
+++ player.rb
@@ -1,20 +1,28 @@
 class Player
-  attr_reader :start_position, :current_position
+  attr_reader :start_position, :current_position, :current_direction
 
   def initialize(rows, columns)
     @length = 1
     @rows = rows - 1
     @columns = columns - 1
     @current_position = start_position
+    @current_direction
   end
 
   def start_position
     [ Random.new.rand(@columns), Random.new.rand(@rows) ]
-
   end
 
   def current_position
     @current_position
+  end
+
+  def current_direction
+    @current_direction
+  end
+
+  def set_current_direction(direction)
+    @current_direction = direction
   end
 
   def set_current_position(x, y)
