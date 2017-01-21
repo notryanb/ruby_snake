aa6bf6d476a4532899d668c8eb6f30c97a9abbe5
notryanb <notryanb@gmail.com>
2017-01-20 20:25:59 -0500

    Refactor getter/setters

--- main.rb
+++ main.rb
@@ -22,13 +22,13 @@ def main
   loop do
     printf "\033c" # Clears Screen to 0,0
 
-    system("stty raw -echo")
+    system('stty raw -echo')
     char = STDIN.read_nonblock(1) rescue nil
     current_position = player.current_position
 
     if player.current_direction == :left
       if player.current_position[0] > 0
-        player.set_current_position(current_position[0] - 1, current_position[1])
+        player.current_position = [current_position[0] - 1, current_position[1]]
         board.board[player.current_position[1]][player.current_position[0]] = player
         board.board[current_position[1]][current_position[0]] = '.'
       end
@@ -36,7 +36,7 @@ def main
 
     if player.current_direction == :right
       if (player.current_position[0] + 1) < columns
-        player.set_current_position(current_position[0] + 1, current_position[1])
+        player.current_position = [current_position[0] + 1, current_position[1]]
         board.board[player.current_position[1]][player.current_position[0]] = player
         board.board[current_position[1]][current_position[0]] = '.'
       end
@@ -44,7 +44,7 @@ def main
 
     if player.current_direction == :down
       if (player.current_position[1] + 1) < rows
-        player.set_current_position(current_position[0], current_position[1] + 1)
+        player.current_position = [current_position[0], current_position[1] + 1]
         board.board[player.current_position[1]][player.current_position[0]] = player
         board.board[current_position[1]][current_position[0]] = '.'
       end
@@ -52,39 +52,36 @@ def main
 
     if player.current_direction == :up
       if (player.current_position[1] - 1) >= 0
-        player.set_current_position(current_position[0], current_position[1] -1)
+        player.current_position = [current_position[0], current_position[1] - 1]
         board.board[player.current_position[1]][player.current_position[0]] = player
         board.board[current_position[1]][current_position[0]] = '.'
       end
     end
 
-    if char =~ /a/i # Left
-      player.set_current_direction(:left)
-    elsif char =~ /d/i #right
-      player.set_current_direction(:right)
-    elsif char =~ /s/i #down
-      player.set_current_direction(:down)
-    elsif char =~ /w/i #up
-      player.set_current_direction(:up)
+    if char =~ /a/i
+      player.current_direction = :left
+    elsif char =~ /d/i
+      player.current_direction = :right
+    elsif char =~ /s/i
+      player.current_direction = :down
+    elsif char =~ /w/i
+      player.current_direction = :up
     end
 
-   
-    system("stty -raw echo")
+    system('stty -raw echo')
 
     break if /q/i =~ char
 
     puts "\n==================\nPRESS 'q' to quit \n"
     puts board.to_s
-    
     puts "Current Position: #{current_position}"
     puts "Player Position: #{player.current_position}"
     puts "Food Position: #{food.current_position}"
 
     sleep 0.2
-  end  
+  end
 
   puts "\n\nThanks for playing"
 end
-
 
 main
--- player.rb
+++ player.rb
@@ -1,33 +1,30 @@
 class Player
-  attr_reader :start_position, :current_position, :current_direction
+  attr_reader :start_position
+  attr_accessor :current_position, :current_direction
 
   def initialize(rows, columns)
-    @length = 1
+    @body = []
     @rows = rows - 1
     @columns = columns - 1
     @current_position = start_position
-    @current_direction
+    @current_direction = ''
   end
 
   def start_position
-    [ Random.new.rand(@columns), Random.new.rand(@rows) ]
+    [Random.new.rand(@columns), Random.new.rand(@rows)]
   end
 
-  def current_position
-    @current_position
+  def grow(cell)
+    @body << cell
   end
 
-  def current_direction
-    @current_direction
-  end
+  #def current_direction=(direction)
+    #@direction = direction
+  #end
 
-  def set_current_direction(direction)
-    @current_direction = direction
-  end
-
-  def set_current_position(x, y)
-    @current_position = [ x, y ]
-  end
+  #def current_position=(coords)
+    #@current_position = coords
+  #end
 
   def to_s
     'X'
