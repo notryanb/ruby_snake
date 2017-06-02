b6eb2b35f37c2b0912fdc2de52f9de7962285be5
Ryan B <notryanb@gmail.com>
2017-06-02 14:50:10 -0400

    Confirm states do change

--- src/models/state_manager.rb
+++ src/models/state_manager.rb
@@ -3,7 +3,7 @@ module GameState
     attr_accessor :current_state, :next_state
 
     def initialize
-      @current_state = GameState::Play.new(30, 30)
+      @current_state = GameState::Menu.new
     end
 
     def next_state
