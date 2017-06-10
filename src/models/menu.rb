2db6f2cda235e5ab0361c2e2604876e0b9de1046
notryanb <notryanb@gmail.com>
2017-06-10 09:28:31 -0400

    Completed is readonly from outside

--- src/models/menu.rb
+++ src/models/menu.rb
@@ -1,10 +1,8 @@
 module GameState
   class Menu
-    attr_accessor :completed
-    attr_reader :name
+    attr_reader :name, :completed
 
     def initialize
-      @player_input = nil
       @completed = false
       @name = :menu
     end
@@ -13,13 +11,13 @@ module GameState
       msg = 'WELCOME TO RUBY SNAKE\n Press <e> to Start'
 
       # Check for user 
-      @player_input = Curses.getch
-      @completed = true if @player_input =~ /e/i
+      player_input = Curses.getch
+      @completed = true if player_input =~ /e/i
 
       Curses.setpos(0, 0)
       Curses.addstr(msg)
-      Curses.refresh
       sleep 0.1
+      Curses.refresh
     end
   end
 end
